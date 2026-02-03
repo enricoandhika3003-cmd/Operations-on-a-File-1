96bd02a905c6458471a44634bd1737f345a713cd
enricoandhika3003-cmd <enrico.andhika3003@gmail.com>
2026-02-03 18:44:53 +0700

    done

---
+++ demo.py
@@ -0,0 +1,44 @@
+file = open('Alphabet.file', 'r')
+print(file.read(6))
+file.close()
+
+file = open('Alphabet.file', 'r')
+print(file.readline())
+print(file.readline())
+file.close()
+
+file = open('Alphabet.file', 'r')
+print(file.readlines())
+file.close()
+
+file = open('Alphabet.file', 'r')
+for x in file:
+    print(x)
+file.close()
+
+file = open('Alphabet.file', 'r')
+file2 = open('Updated_Alphabet.file', 'w')
+
+for line in file.readlines():
+    if not (line.startswith('-')):
+        print(line)
+        file2.write(line)
+
+file.close()
+file2.close()
+
+file = open('Alphabet.file', 'r')
+file2 = open('Copy.file', 'w')
+
+cont = file.readline()
+type(cont)
+for i in range (1, len(cont),+1):
+    if (i%2 != 0):
+        print(cont)
+        file2.write(cont)
+        pass
+
+file.close()
+file2.close()
+
+    
